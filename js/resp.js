455cd0528666046dd6961504ce01a637891c8c88
PacificAgni <140973461+PacificAgni@users.noreply.github.com>
2023-07-30 23:29:19 +0530

    resp.js

--- js/resp.js
+++ js/resp.js
@@ -1,1 +1,30 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navlist=document.querySelector('.nav-list')
+rightnav=document.querySelector('.right-nav');
+anchor=document.querySelector('.anchor');
+
+anchor.addEventListener('click',()=>{
+    rightnav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+burger.addEventListener('click',()=>{
+    rightnav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
+ // Optional: If you want to add a smooth scroll effect to all anchor tags
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
